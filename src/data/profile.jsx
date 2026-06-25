822015ea9e42c137c549919f5924ba0beb168fd7
Yoann <yoann.donizetti@gmail.com>
2026-06-25 13:00:54 +0200

    suppression cv pour remplacement

--- src/data/profile.jsx
+++ src/data/profile.jsx
@@ -6,7 +6,7 @@
     "Conception de solutions Data, IA et Machine Learning orientées métier.",
 
   description:
-    <>En reconversion vers la <span className="highlight">Data Science</span>, je crée des solutions mêlant <span className="highlight">analyse de données</span>, <span className="highlight">IA</span>, <span className="highlight">systèmes de recommandation</span> et <span className="highlight">MLOps</span>  pour répondre à des besoins métier concrets avec des outils fiables et évolutifs.</>,
+    <>Formé en  <span className="highlight">Data Science</span>, je crée des solutions mêlant <span className="highlight">analyse de données</span>, <span className="highlight">IA</span>, <span className="highlight">systèmes de recommandation</span> et <span className="highlight">MLOps</span>  pour répondre à des besoins métier concrets avec des outils fiables et évolutifs.</>,
 
   github: "https://github.com/yoann-donizetti",
 
